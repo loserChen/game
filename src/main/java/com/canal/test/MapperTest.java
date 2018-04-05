c6b4ba214d3f7d4ec3c0dfec394984defdc983aa
loserChen <chenzyfm@outlook.com>
2018-04-05 18:55:29 +0800

    second commit

--- src/main/java/com/canal/test/MapperTest.java
+++ src/main/java/com/canal/test/MapperTest.java
@@ -1,6 +1,8 @@
 package com.canal.test;
 
+import com.canal.mapper.PetMapper;
 import com.canal.mapper.UserMapper;
+import com.canal.po.Pet;
 import com.canal.po.User;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
@@ -9,26 +11,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Random;
 import java.util.UUID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext-*.xml"})
 public class MapperTest {
     @Autowired
-    private UserMapper userMapper;
-    @Autowired
     private SqlSession sqlSession;
+    @Autowired
+    private PetMapper petMapper;
     @Test
     public void text(){
-       UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
-        for(int i=0;i<100;i++){
-            User user=new User();
-            user.setGold(0);
-            user.setPassword("123456");
-            String uid= UUID.randomUUID().toString().substring(0,5)+i;
-            user.setUsername(uid+i);
-            userMapper.insertSelective(user);
+       PetMapper petMapper=sqlSession.getMapper(PetMapper.class);
+        for(int i=0;i<200;i++){
+            Pet pet=new Pet();
+            Random random = new Random();
+            int hp=random.nextInt(81)+20;
+            int attack=random.nextInt(81)+20;
+            int defence=random.nextInt(81)+20;
+            int specialAttack=random.nextInt(81)+20;
+            int specialDefence=random.nextInt(81)+20;
+            int speed=random.nextInt(81)+20;
+            int total=hp+attack+defence+specialAttack+speed;
+            String image=random.nextInt(20)+".jpg";
+            pet.setHp((byte)hp);
+            pet.setAttack((byte)attack);
+            pet.setDefence((byte)defence);
+            pet.setSpecialAttack((byte)specialAttack);
+            pet.setSpecialDefence((byte)specialDefence);
+            pet.setSpeed((byte)speed);
+            pet.setTotal((short)total);
+            pet.setImage(image);
+            pet.setUserId(i+1);
+            petMapper.insertSelective(pet);
         }
-        System.out.println(userMapper);
     }
 }
---
+++ target/game/jsp/login.jsp
@@ -0,0 +1,31 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chenzeyuan
+  Date: 2018/4/5
+  Time: 下午4:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>我的精灵</title>
+    <link href="${pageContext.request.contextPath }/static/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="${pageContext.request.contextPath }/static/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="static/css/game.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <img src="/pic/czy.JPG" alt="头像" class="img-circle" style="height: 150px">
+        </div>
+        <div class="col-md-3">
+
+        </div>
+    </div>
+</div>
+<script src="${pageContext.request.contextPath }/static/js/jquery-1.12.4.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="${pageContext.request.contextPath }/static/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
